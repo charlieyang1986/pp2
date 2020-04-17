5425551ec5a8b091fad7d04af1797076eaa4ff65
Chunhai Yang <cyang21@mail.greenriver.edu>
2020-04-17 16:38:53 -0400

    step 1

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+/*
+ *Name: Chunhai Yang
+ * Date: 4/17/2020
+ * URL: https://github.com/charlieyang1986/pp2.git
+ * Pair Program 2
+ */
+
+
+echo "PHP Array Practice";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 2</title>
+</head>
+<body>
+      <h1>Pair Program 2</h1>
+</body>
+</html>
